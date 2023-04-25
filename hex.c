e0d9d31e4a434b431ed16469a1779202e76c8bcc
bazirakye <bazirakyetonny15@gmail.com>
2023-04-25 09:28:43 -0700

    hex

--- hex.c
+++ hex.c
@@ -65,7 +65,7 @@ unsigned int convert_X(va_list args, buffer_t *output,
 	if (len == SHORT)
 		num = (unsigned short)num;
 	if (HASH_FLAG == 1 && num != 0)
-		ret += memcpy(output, lead, 2);
+		ret += _memcpy(output, lead, 2);
 	if (!(num == 0 && prec == 0))
 		ret += convert_ubase(output, num, "0123456789ABCDEF",
 				flags, wid, prec);
